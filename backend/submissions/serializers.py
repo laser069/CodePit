dc1b7ba17ccda2a5c675721e0e7888765102caa9
Sachin-2006 <sachin06kaizen@gmail.com>
2025-08-17 16:02:31 +0530

    Serialized the submission model

---
+++ backend/submissions/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import SubmissionModel
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubmissionModel
+        fields = [id,"user","problem","code","language","status"]
+
+        read_only_fields = ["user"]
+
+
+
+
+
+
+
+
+
+
